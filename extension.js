e3501904e27bae04d0f740529fa617bd13fed10e
Arshad Khan <muhammadarshad0910@gmail.com>
2024-09-27 05:04:03 +0100

    setup different editting options. Added new visual options

--- extension.js
+++ extension.js
@@ -10,7 +10,10 @@ function activate(context) {
             const document = editor.document;
             const filePath = document.fileName;
 
-            if (path.extname(filePath) === '.yaml' || path.extname(filePath) === '.yml') {
+            if (path.extname(filePath) === '.yaml' || 
+				path.extname(filePath) === '.yml' || 
+				path.extname(filePath) === '.tmap2'
+					) {
                 const fileContent = document.getText();
                 const data = yaml.load(fileContent);
 				if ( data.nodes !== undefined ){
@@ -66,22 +69,69 @@ function handle_convert_yaml_to_topomap(data) {
 	graph_data = data.nodes.map( (node, index) => {
 		const { x, y, z } = node.node.pose.position 
 		return { 
-			data :  {id : node.meta.node, node : node },
-			position : { x, y }
+			data :  {id : node.meta.node, node : node  },
+			position : { x, y }, 
+			classes : "topological_node"
 		}
 	})
 
+	// add the vertices 
+	data.nodes.forEach( (node )=> {
+		const { x, y } = node.node.pose.position 
+		const node_id = node.meta.node
+		const verts = node.node.verts 
+		for ( let i = 0; i < verts.length; i++ ){
+			const vert = verts[i]
+			graph_data.push( { 
+				data : {id : `${node_id}_${i}`, }, 
+				position : { x :  x + vert.x, y : y + vert.y },
+				classes : "topological_vert"
+			})
+
+			if ( i > 0 ) {
+				let i_prev = i - 1
+				graph_data.push( { 
+					data : {
+						id : `${node_id}_${i}_${i_prev}`, 
+						source : `${node_id}_${i}`, 
+						target : `${node_id}_${i_prev}`,
+						weight : 1
+					}, 
+					classes : "topological_vert_edge"
+				})
+				
+				if  (i === verts.length - 1 ){
+					i_prev = 0
+					graph_data.push( { 
+						data : {
+							id : `${node_id}_${i}_${i_prev}`, 
+							source : `${node_id}_${i}`, 
+							target : `${node_id}_${i_prev}`,
+							weight : 1
+						}, 
+						classes : "topological_vert_edge"
+					})
+				}
+			}
+		}
+	})
+
+
+	// for edges 
 	for ( let i = 0; i < data.nodes.length; i++ ){
 		const node = data.nodes[i] 
 		for ( let j = 0; j < node.node.edges.length; j++ ){
 			const edge = node.node.edges[j] 
-			graph_data.push( { data : {
-				id : edge.edge_id, 
-				source : node.meta.node,
-				target : edge.node, 
-				edge : edge ,
-				weight: 1,
-			} } )
+			graph_data.push( { 
+				data : {
+					id : edge.edge_id, 
+					source : node.meta.node,
+					target : edge.node, 
+					edge : edge ,
+					weight: 1,
+				}, 
+				classes : "topological_edge"
+			})
 		}
 	}
 
@@ -109,27 +159,52 @@ function handle_generate_webview(uris, graph_data) {
 		<body>
 			<div id="side-bar"> 
 
-				<input type="checkbox" id="lock" name="lock" >
-				<label for="lock"> Lock View</label>
 
-				<input type="checkbox" id="edit" name="edit" >
-				<label for="edit"> Edit Graph</label>
-				<br>
+				<div class="sidebar-sub-container">
+					<input type="checkbox" id="lock" name="lock" >
+					<label for="lock"> Lock View</label>
+					<input type="checkbox" id="edit" name="edit" >
+					<label for="edit"> Edit Graph</label>
+					<input type="checkbox" id="hide-vert" name="hide-vert" >
+					<label for="hide-vert"> Hide Verts </label>
+					<br>
 
-				<label for="source">Source node:</label>
-				<select name="source" id="source">
-				</select>
-				<br>
-
-				<label for="target">Target node:</label>
-				<select name="target" id="target">
-				</select>
-				<br>
-
-				<button id="find-path"> Find Path </button>
-				<button id="clear-path"> Clear Path </button>
+					<input type="checkbox" id="hide-edge-names" name="hide-edge-names" >
+					<label for="hide-edge-names"> Hide Edge Labels</label>
 					
+					<input type="checkbox" id="hide-node-names" name="hide-node-names" >
+					<label for="hide-node-names"> Hide Node Labels</label>
+					<br>
+					
+
+				</div>
+
+				<div class="sidebar-sub-container">
+					<button id="center-graph-view"> Center Graph View </button>
+					<button id="export=datum-file"> Export Topological Map </button>
+				</div>
+
+
+				<div class="sidebar-sub-container">
+					<label for="source">Source node:</label>
+					<select name="source" id="source">
+					</select>
+
+					<label for="target">Target node:</label>
+					<select name="target" id="target">
+					</select>
+					<br>
+
+					<div>
+						<button id="find-path"> Find Path </button>
+						<button id="clear-path"> Clear Path </button>
+					</div>
+
+				</div>
+				
 			</div>
+
+			<div id="node-modal" class="node-modal"></div>
 			<div id="cy" style="width: 100%; height: 100%;"></div>
 
 			<script>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+const TOPOLOGICAL_NODE_COLOR = "rgba(204,204,204, 0.4)"
+const TOPOLOGICAL_EDGE_COLOR = "rgba(25,100,23,0.5)"
+const TOPOLOGICAL_VERT_NODE_COLOR = "rgba(204,204,204, 0.4)"
+const TOPOLOGICAL_VERT_EDGE_COLOR = "rgba(255,0,0,1.0)"
+
 class GraphHandler
 {
     constructor(){
@@ -6,6 +11,8 @@ class GraphHandler
 
         this._source_node = null
         this._target_node = null
+
+        this._a_start_result = null
 
         this._is_graph_rotating = false 
         this._start_x = 0 
@@ -21,31 +28,68 @@ class GraphHandler
             elements: graph_data,
             style: [
                 {
-                    selector: 'node',
+                    selector: '.topological_node',
                     style: {
-                        'background-color': '#666',
+                        'background-color': TOPOLOGICAL_NODE_COLOR, //'#666',
                         'label': 'data(id)',
                         'text-rotation' : 0,
                         'font-size' : 0.15,
-                        'color' : 'rgba(204,204,204, 0.4)',
+                        'color' : TOPOLOGICAL_NODE_COLOR,
                         'height' : 0.2, 
                         'width' : 0.2,
                     }
                 },
                 {
-                    selector: 'edge',
+                    selector: '.topological_edge',
                     style: {
-                        'width': 0.05,
-                        'line-color': 'rgba(25,100,23,0.5)',
+                        'line-color': TOPOLOGICAL_EDGE_COLOR,
+                        'color' : TOPOLOGICAL_NODE_COLOR,
+                        'width': 0.085,
+                        'label': 'data(id)',
+                        'font-size' : 0.075,
+                        'target-arrow-shape': 'triangle',
+                        'text-rotation': 'autorotate',   
+
                     }
                 },
                 {
-                    selector: ':selected',
+                    selector: '.topological_vert',
+                    style: {
+                        'color' : TOPOLOGICAL_VERT_NODE_COLOR,
+                        'height' : 0.01, 
+                        'width' : 0.01,
+                    }
+                },
+                {
+                    selector: '.topological_vert_edge',
+                    style: {
+                        'line-color': TOPOLOGICAL_VERT_EDGE_COLOR,
+                        'width': 0.01,
+                    }
+                },
+                {
+                    selector: '.hidden-label',
+                    style: {
+                        'label': '' 
+                    }
+                },
+                {
+                    selector: '.hidden-verts',
+                    style: {
+                        'label': '',
+                        'height' : 0.0, 
+                        'width' : 0.0,
+                        'color' : "transparent",
+                    }
+                },
+                {
+                    selector: '.shortest-path',
                     css: {
-                        'background-color': '#00FF00',
+                        'background-color': '#FF0000',
                         'line-color': 'rgba(255,0,0,1.0)',
-                        'height' : 0.1, 
-                        'width' : 0.1,
+                        'height' : 0.25, 
+                        'width' : 0.25,
+                        "z-index" : 2
                     }
                 }
             ],
@@ -58,10 +102,11 @@ class GraphHandler
         });
 
         this.handle_setup_events()
-        this.handle_dom_events()
+        this.handle_dom_events_control_bar()
+        this.handle_setup_dom_event_path_find()
     }
 
-    handle_dom_events()
+    handle_dom_events_control_bar()
     {
         const lock_screen = document.getElementById("lock")
         lock_screen.addEventListener('click', ()=>{
@@ -78,8 +123,60 @@ class GraphHandler
             this._graph.autoungrabify( !edit_graph.checked )
         })
 
+        const hide_verts = document.getElementById("hide-vert")
+        hide_verts.addEventListener("click", (event)=> {
+            
+            if ( event.target.checked ){
+                this._graph?.nodes('.topological_vert').addClass("hidden-verts")
+                this._graph?.edges('.topological_vert_edge').addClass("hidden-verts")
+            } else { 
+                this._graph?.nodes('.topological_vert').removeClass("hidden-verts")
+                this._graph?.edges('.topological_vert_edge').removeClass("hidden-verts")
+            }
+
+        })
+
+        const hide_edge_names = document.getElementById("hide-edge-names")
+        hide_edge_names.addEventListener("click", (event)=> {
+            if ( event.target.checked ){
+                this._graph?.edges().addClass("hidden-label")
+            } else { 
+                this._graph?.edges().removeClass("hidden-label")
+            }
+        })
+
+        const hide_node_names = document.getElementById("hide-node-names")
+        hide_node_names.addEventListener("click", (event)=> {
+            if ( event.target.checked ){
+                this._graph?.nodes().addClass("hidden-label")
+            } else { 
+                this._graph?.nodes().removeClass("hidden-label")
+            }
+        })
+
+
+        // buttons 
+        const btn_center_graph = document.getElementById("center-graph-view")
+        btn_center_graph.addEventListener("click",  ()=>{
+            this._graph?.fit()
+        })
+
+
+
+    }
+
+    handle_setup_dom_event_path_find()
+    {
+        // filter nodes (remove vert-node) -> sort -> append to selector.
         const source_selector = document.getElementById('source')
-        this._graph.nodes().forEach(node => {
+        this._graph.nodes()
+        .filter( (a)=> { return a.data('node') })
+        .sort( (a, b)=> {  
+            if ( a.id() > b.id() ) { return 1 }
+            if ( a.id() < b.id() ) { return -1 }
+            return 0;
+        })
+        .forEach(node => {
             const opt = document.createElement('option');
             opt.value = node.id();
             opt.innerHTML = node.id();
@@ -87,7 +184,14 @@ class GraphHandler
         })  
 
         const target_selector = document.getElementById('target')
-        this._graph.nodes().forEach(node => {
+        this._graph.nodes()
+        .filter( (a)=> { return a.data('node') })
+        .sort( (a, b)=> {  
+            if ( a.id() > b.id() ) { return 1 }
+            if ( a.id() < b.id() ) { return -1 }
+            return 0;
+        })
+        .forEach(node => {
             const opt = document.createElement('option');
             opt.value = node.id();
             opt.innerHTML = node.id();
@@ -96,52 +200,109 @@ class GraphHandler
 
         const find_path_btn = document.getElementById("find-path")
         find_path_btn.addEventListener( 'click', ()=>{
-            this._graph.elements().filter( i => { return i.selected() }).unselect()
+            this._graph.nodes().removeClass("shortest-path")
+            this._a_start_result?.path?.removeClass("shortest-path")
+
             this._source_node = this._graph.getElementById(source_selector.value)
             this._target_node = this._graph.getElementById(target_selector.value)
-            const a_start_results = this._graph.elements().aStar({ root: this._source_node, goal: this._target_node });
-            a_start_results.path.select()
-
+            this._a_start_result = this._graph.elements().aStar({ root: this._source_node, goal: this._target_node });
+            if ( this._a_start_result.found ) {
+                this._a_start_result.path?.addClass("shortest-path")
+            }
         })
         
         const clear_path_btn = document.getElementById("clear-path")
         clear_path_btn.addEventListener( 'click', ()=>{
-            this._graph.elements().filter( i => { return i.selected() }).unselect()
+            this._a_start_result?.path?.removeClass("shortest-path")
         })
     }
 
 
     handle_setup_events()
     { 
-        this._graph.on('mousedown', (event) => {
-            if (event.originalEvent.shiftKey) { // Hold Shift to rotate
-                this._is_graph_rotating = true;
-                this._start_x = event.originalEvent.clientX;
-                this._start_y = event.originalEvent.clientY;
-            }
-        });
-        
-        this._graph.on('mousemove', (event) => {
-            if (this._is_graph_rotating) {
-                const currentX = event.originalEvent.clientX;
-                const currentY = event.originalEvent.clientY;
-                const deltaX = currentX - this._start_x;
-                const deltaY = currentY - this._start_y;
-                const angle = Math.atan2(deltaY, deltaX);
-                const rotateBy = angle - this._initial_angle;
-                this._initial_angle = angle;
-                this._start_x = currentX;
-                this._start_y = currentY;
-            }
-        });
-        
-        this._graph.on('mouseup', () => {
-            if (this._is_graph_rotating) {
-                this._is_graph_rotating = false;
-            }
+
+        this._graph.on("position", ".topological_node", (event)=> {
+            const node = event.target 
+            this.handle_show_node_modal(node)
+            this.handle_move_node_verts(node)
+        })
+
+        this._graph.on("position", ".topological_vert", (event)=> {
+            const node = event.target 
+            this.handle_update_node_vert(node)
+        })
+
+        this._graph.on('dragfreeon', 'node', (event)=> {
+            this.handle_hide_node_modal();
         });
     }
 
+
+    // Function to show modal and update its content
+    handle_show_node_modal(node) {
+
+        const { x, y } = node.position()
+        const modal = document.getElementById('node-modal');
+        modal.style.display = 'block';
+
+        const edges = node.data("node").node.edges 
+        const edges_html = edges.map((e)=> { return `<li> ${e.edge_id} </li>`}).join(" ")
+
+        modal.innerHTML = `
+            <b>Node ID:</b> ${node.id()} <br>
+            <b>Position (xy):</b>  ${x.toFixed(4)}  Y: ${y.toFixed(4)} <br>
+            <b>Edges:</b>${edges_html}
+        `;
+    
+        // Update modal position to follow the node
+        modal.style.left = `${parseInt(node.renderedPosition().x) + 10}px`; 
+        modal.style.top = `${parseInt(node.renderedPosition().y) - 20}px`;
+    }
+    
+
+    // node: topo-node
+    handle_move_node_verts(node){
+
+        const node_id = node.id()
+        const node_data = node.data("node")
+        const verts = node_data.node.verts 
+
+        const { x, y } = node.position()
+        for ( let index = 0; index < verts.length ; index ++ ){
+            const vert = verts[index]
+            const id = `${node_id}_${index}`
+            const vert_node = this._graph.getElementById(id)
+            if ( vert_node ) { 
+                vert_node.position({
+                    x : x + vert.x, 
+                    y : y + vert.y
+                })
+            }
+        }
+    }
+
+    // node: vert-node
+    handle_update_node_vert(node){
+        const node_id = node.id()
+
+        if ( node_id.split("_").length === 2 ){
+            
+            const parent_node_id = node_id.split("_")[0]
+            const child_vert_id =  node_id.split("_")[1]
+
+            const parent_node = this._graph?.getElementById(parent_node_id)
+            const parent_node_data = parent_node?.data('node')
+            const parent_position = parent_node.position()
+            const child_position = node.position()
+
+            parent_node_data.node.verts[ parseInt(child_vert_id) ] = { x : child_position.x - parent_position.x, y : child_position.y - parent_position.y }
+            parent_node.data( 'node', parent_node_data )
+        }
+    }
+
+    handle_hide_node_modal(){
+        document.getElementById('node-modal').style.display = 'none';
+    }
 }
 
 
